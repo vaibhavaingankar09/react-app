457d482b4c6233c9b11c3260fb850ef90ef4c7c4
Vaibhav Vaingankar <vaibhav_vaingankar@persistent.co.in>
2019-07-12 15:43:43 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I'm a React App </h1>
+      <p> This is really working!</p>
+      <Person name="vaibhav" age="26" />
+      <Person name="neeraj" age="27">
+        My Hobbies: Biking
+      </Person>
+      <Person name="joey" age="27" />
     </div>
   );
+  // return React.createElement(
+  //   "div",
+  //   { className: "App" },
+  //   React.createElement("h1", null, "Hi, I'm a React App2")
+  // );
 }
 
 export default App;
